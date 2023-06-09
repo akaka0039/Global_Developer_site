de18be7883d6a3236197dd830de2184aad45abe0
aka0039 <yumeno.13579@gmail.com>
2023-06-09 12:16:15 -0700

    fix events tables

--- src/database/migrations/2023_05_27_054054_create_events_table.php
+++ src/database/migrations/2023_05_27_054054_create_events_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('address');
             $table->string('imagePath');
             $table->text('description');
-            $table->string('limitMember');
             $table->timestamps();
         });
     }
