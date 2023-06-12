0f40e8d6a434b527e3ad01870f9d732286a6bc47
aka0039 <yumeno.13579@gmail.com>
2023-06-12 12:47:48 -0700

    fix #61 & #64

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; 
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            DB::table('users')->insert([
+                'name' => 'User' . $i,
+                'introduction' => 'Introduction' . $i,
+                'image' => 'image1.jpg',
+                'habitation' => 'Habitation' . $i,
+                'nationality' => 'Nationality' . $i,
+                'position' => 'Position' . $i,
+                'url' => 'example.com/user' . $i,
+                'attendance_count' => $i,
+                'email' => 'user'. $i . '@example.com',
+                'password' => Hash::make('password' .$i),
+                'email_verified_at' => now(),
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
