fb2563b3007f1f201d1d3494f7cdafbc8bcc9e72
shin <5446shin@gmail.com>
2023-06-13 16:08:47 -0700

    Connect with backend for creating event

--- src/resources/js/Pages/Event/TimeForm.jsx
+++ src/resources/js/Pages/Event/TimeForm.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 
-const TimeForm = () => {
+const TimeForm = (props) => {
     const [startTime, setStartTime] = useState("");
     const [endTime, setEndTime] = useState("");
+    const errorMessageStyle = "text-red-500 italic text-lg";
 
+    const handleStartTime = (event) => {
+        const newValue = event.target.value;
+        setStartTime(newValue);
+        props.handleStartTimeChange(newValue);
+    };
+    const handleEndTime = (event) => {
+        const newValue = event.target.value;
+        setEndTime(newValue);
+        props.handleEndTimeChange(newValue);
+    };
     return (
         <div className="pd-4">
             <div className="pd-4">
@@ -14,9 +25,10 @@ const TimeForm = () => {
                     className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     type="time"
                     value={startTime}
-                    onChange={(e) => setStartTime(e.target.value)}
+                    onChange={handleStartTime}
                 />
             </div>
+            {props.errors.start_date && <div className={errorMessageStyle}>{props.errors.start_date}</div> }
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                     End Time
@@ -25,9 +37,10 @@ const TimeForm = () => {
                     className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     type="time"
                     value={endTime}
-                    onChange={(e) => setEndTime(e.target.value)}
+                    onChange={handleEndTime}
                 />
             </div>
+            {props.errors.end_date && <div className={errorMessageStyle}>{props.errors.end_date}</div> }
         </div>
     );
 };
