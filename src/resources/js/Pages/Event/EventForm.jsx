315983e9eb87f6ded32ecada3796c5944d364fe2
aka0039 <yumeno.13579@gmail.com>
2023-06-14 12:05:22 -0700

    fix issue #76

--- src/resources/js/Pages/Event/EventForm.jsx
+++ src/resources/js/Pages/Event/EventForm.jsx
@@ -27,6 +27,10 @@ const EventForm = ({ errors, auth }) => {
         setImage(file);
     };
 
+    const handleUploadClick = () => {
+        document.getElementById("image-upload").click();
+    };
+
     const handleDragOver = (e) => {
         e.preventDefault();
         setIsDragging(true);
@@ -47,201 +51,194 @@ const EventForm = ({ errors, auth }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (e.nativeEvent.submitter.id !== "back-button") {
-            const data = {
-                name: name,
-                address: address,
-                participant_limit_number: limit,
-                image: image,
-                description: description,
-                start_date: startTime,
-                end_date: endTime,
-                // Before deploying, should fix this
-                user_id: auth?.user?.user_id,
-            };
-            router.post(`/events`, data);
-        }
+        const data = {
+            name: name,
+            address: address,
+            participant_limit_number: limit,
+            image: image,
+            description: description,
+            start_date: startTime,
+            end_date: endTime,
+            // Before deploying, should fix this
+            user_id: auth?.user?.user_id,
+        };
+        router.post(`/events`, data);
     };
     return (
         <GeneralLayout auth={auth}>
             <div className="flex justify-center items-center">
                 <div className="w-9/12 pt-46">
-                    <form onSubmit={handleSubmit}>
-                        <h1 className="text-2xl font-bold mb-4">
-                            Event Details
-                        </h1>
-                        <div className="bg-white shadow-md rounded px-8 py-6 mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Event Name
-                            </label>
-                            <input
-                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                type="text"
-                                placeholder="Enter event name"
-                                id="name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                            {errors.name && (
-                                <div className={errorMessageStyle}>
-                                    {errors.name}
-                                </div>
-                            )}
-                        </div>
-                        <div
-                            className={`bg-white shadow-md rounded px-8 py-6 mb-4 ${
-                                isDragging ? "border-4 border-blue-300" : ""
-                            }`}
-                            onDrop={handleDrop}
-                            onDragOver={handleDragOver}
-                            onDragLeave={handleDragLeave}
-                        >
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Image
-                            </label>
-                            <div className="mb-4">
-                                {image ? (
-                                    <img
-                                        className="w-full sm:w-48 h-auto object-cover rounded"
-                                        src={URL.createObjectURL(image)}
-                                        alt="Event"
-                                    />
-                                ) : (
-                                    <div className="w-full sm:w-48 sm:h-48 h-auto bg-gray-200 border border-gray-400 rounded flex items-center justify-center">
-                                        <p className="text-gray-500">
-                                            Drag &amp; drop an image
-                                        </p>
-                                    </div>
-                                )}
-                            </div>
-
-                            <input
-                                type="file"
-                                accept="image/*"
-                                className="hidden"
-                                onChange={handleImageChange}
-                            />
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                onClick={() => {
-                                    document
-                                        .getElementById("image-upload")
-                                        .click();
-                                }}
-                            >
-                                Upload Image
-                            </button>
-                        </div>
-
-                        <div className="bg-white shadow-md rounded px-8 py-6 mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Event Date
-                            </label>
-                            <input
-                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                type="date"
-                            />
-                        </div>
-                        <TimeForm
-                            handleStartTimeChange={handleStartTimeChange}
-                            handleEndTimeChange={handleEndTimeChange}
-                            errors={errors}
+                    <h1 className="text-2xl font-bold mb-4">Event Details</h1>
+                    <div className="bg-white shadow-md rounded px-8 py-6 mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Event Name
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="text"
+                            placeholder="Enter event name"
+                            id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
+                        {errors.name && (
+                            <div className={errorMessageStyle}>
+                                {errors.name}
+                            </div>
+                        )}
+                    </div>
+                    <div
+                        className={`bg-white shadow-md rounded px-8 py-6 mb-4 ${
+                            isDragging ? "border-4 border-blue-300" : ""
+                        }`}
+                        onDrop={handleDrop}
+                        onDragOver={handleDragOver}
+                        onDragLeave={handleDragLeave}
+                    >
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Image
+                        </label>
                         <div className="mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Description
-                            </label>
-                            <textarea
-                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                rows={4}
-                                placeholder="Enter your message"
-                                id="description"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                            ></textarea>
-                            {errors.description && (
-                                <div className={errorMessageStyle}>
-                                    {errors.description}
-                                </div>
-                            )}
-                        </div>
-                        <div>
-                            <label
-                                htmlFor="location"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                Location
-                            </label>
-                            <input
-                                type="text"
-                                id="location"
-                                name="location"
-                                className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                placeholder="Enter the location"
-                                value={address}
-                                onChange={(e) => setAddress(e.target.value)}
-                            />
-                            {errors.address && (
-                                <div className={errorMessageStyle}>
-                                    {errors.address}
+                            {image ? (
+                                <img
+                                    className="w-full sm:w-48 h-auto object-cover rounded"
+                                    src={URL.createObjectURL(image)}
+                                    alt="Event"
+                                />
+                            ) : (
+                                <div className="w-full sm:w-48 sm:h-48 h-auto bg-gray-200 border border-gray-400 rounded flex items-center justify-center">
+                                    <p className="text-gray-500">
+                                        Drag &amp; drop an image
+                                    </p>
                                 </div>
                             )}
                         </div>
 
-                        <div className="mt-4">
-                            <label
-                                htmlFor="onlineUrl"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                Online URL
-                            </label>
-                            <input
-                                type="text"
-                                id="onlineUrl"
-                                name="onlineUrl"
-                                className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                placeholder="Enter the online URL"
-                                // value={onlineUrl}
-                                // onChange={(e) => setOnlineUrl(e.target.value)}
-                            />
-                        </div>
-                        <div className="bg-white shadow-md rounded px-8 py-6 mb-4">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">
-                                Attendance Limit
-                            </label>
-                            <input
-                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                type="number"
-                                placeholder="Enter limit"
-                                id="limit"
-                                value={limit}
-                                onChange={(e) => setLimit(e.target.value)}
-                            />
-                            {errors.participant_limit_number && (
-                                <div className={errorMessageStyle}>
-                                    {errors.participant_limit_number}
-                                </div>
-                            )}
-                        </div>
-                        <div className="flex justify-end">
-                            <button
-                                id="back-button"
-                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded  focus:outline-none focus:shadow-outline"
-                                onClick={() => {
-                                    router.visit("/events");
-                                }}
-                            >
-                                Back
-                            </button>
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            onClick={() => {
+                                document.getElementById("image-upload").click();
+                            }}
+                        >
+                            Upload Image
+                        </button>
+                        <input
+                            id="image-upload"
+                            type="file"
+                            accept="image/*"
+                            className="hidden"
+                            onChange={handleImageChange}
+                        />
+                    </div>
 
-                            <button
-                                type="submit"
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
-                            >
-                                Save
-                            </button>
-                        </div>
-                    </form>
+                    <div className="bg-white shadow-md rounded px-8 py-6 mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Event Date
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="date"
+                        />
+                    </div>
+                    <TimeForm
+                        handleStartTimeChange={handleStartTimeChange}
+                        handleEndTimeChange={handleEndTimeChange}
+                        errors={errors}
+                    />
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Description
+                        </label>
+                        <textarea
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            rows={4}
+                            placeholder="Enter your message"
+                            id="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></textarea>
+                        {errors.description && (
+                            <div className={errorMessageStyle}>
+                                {errors.description}
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="location"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Location
+                        </label>
+                        <input
+                            type="text"
+                            id="location"
+                            name="location"
+                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            placeholder="Enter the location"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                        {errors.address && (
+                            <div className={errorMessageStyle}>
+                                {errors.address}
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="mt-4">
+                        <label
+                            htmlFor="onlineUrl"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Online URL
+                        </label>
+                        <input
+                            type="text"
+                            id="onlineUrl"
+                            name="onlineUrl"
+                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            placeholder="Enter the online URL"
+                            // value={onlineUrl}
+                            // onChange={(e) => setOnlineUrl(e.target.value)}
+                        />
+                    </div>
+                    <div className="bg-white shadow-md rounded px-8 py-6 mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">
+                            Attendance Limit
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="number"
+                            placeholder="Enter limit"
+                            id="limit"
+                            value={limit}
+                            onChange={(e) => setLimit(e.target.value)}
+                        />
+                        {errors.participant_limit_number && (
+                            <div className={errorMessageStyle}>
+                                {errors.participant_limit_number}
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex justify-end">
+                        <button
+                            id="back-button"
+                            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded  focus:outline-none focus:shadow-outline"
+                            onClick={() => {
+                                router.visit("/events");
+                            }}
+                        >
+                            Back
+                        </button>
+                        <button
+                            type="button"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
+                            onClick={handleSubmit}
+                        >
+                            Save
+                        </button>
+                    </div>
                 </div>
             </div>
         </GeneralLayout>
