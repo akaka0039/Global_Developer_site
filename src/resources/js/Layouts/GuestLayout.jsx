dab586ccc54df79a6b30194f37b730ce6fa90aa1
aka0039 <yumeno.13579@gmail.com>
2023-06-16 16:17:49 -0700

    fix lauout

--- src/resources/js/Layouts/GuestLayout.jsx
+++ src/resources/js/Layouts/GuestLayout.jsx
@@ -1,21 +1,22 @@
-import { Link } from "@inertiajs/react";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
+import backgroundImage from "/public/images/top_background.jpg";
 
 export default function Guest({ children }) {
+    const guestStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    };
     return (
-        <>
-            <div>
-                <Header />
-            </div>
-            <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div className="min-h-screen" style={guestStyle}>
+            <Header />
+            <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white bg-opacity-75 shadow">
                 <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                     {children}
                 </div>
             </div>
-            <div>
-                <Footer />
-            </div>
-        </>
+            <Footer />
+        </div>
     );
 }
