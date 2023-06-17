722796980adc89881ec42d37f93be380a89d93b7
shin <5446shin@gmail.com>
2023-06-17 10:50:26 -0700

    Build front end for tags

--- src/resources/js/Pages/Event/EventDetail.jsx
+++ src/resources/js/Pages/Event/EventDetail.jsx
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 import { router } from "@inertiajs/react";
 import GeneralLayout from "@/Layouts/GeneralLayout";
+import axios from "axios";
 
-const EventDetail = ({ auth, event, is_attend, participants }) => {
+const EventDetail = ({ auth, event, is_attended, participants, tags }) => {
     // for debug
-    console.log(event, is_attend, participants);
+    console.log(event, is_attended, participants, tags);
 
-    function handleEditClick() {
+    const [isAttended, SetIsAttended] = useState(is_attended);
+    const [participantsState, SetParticipantsState] = useState(participants);
+
+    const handleEditClick = () => {
         // console.log(event.event_id);
         router.get(`/events/${event.event_id}/edit`);
     }
 
-    function handleAttendClick() {
-        router.post(`/events/${event.event_id}/participants`, {user_id: auth?.user?.user_id});
+    const handleAttendClick = async () => {
+        const response = await axios.post(`/events/${event.event_id}/participants`, { user_id: auth?.user?.user_id });
+        if (response?.data?.is_attended) {
+            SetIsAttended(true);
+        }
+        if (response?.data?.participants) {
+            SetParticipantsState(response?.data?.participants);
+        }
     }
 
-    function handleNotAttendClick() {
-        router.delete(`/events/${event.event_id}/participants`, {user_id: auth?.user?.user_id});
+    const handleNotAttendClick = async () => {
+        const response = await axios.delete(`/events/${event.event_id}/participants`, { user_id: auth?.user?.user_id });
+        if (!response?.data?.is_attended) {
+            SetIsAttended(false);
+        }
+        if (response?.data?.participants) {
+            SetParticipantsState(response?.data?.participants);
+        }
     }
 
     return (
@@ -40,16 +56,28 @@ const EventDetail = ({ auth, event, is_attend, participants }) => {
                             />
                             <div className="absolute inset-0 bg-black opacity-25"></div>
                         </div>
-                        <div className="px-6 py-8 sm:px-10">
-                            <h1 className="text-4xl leading-8 font-bold text-gray-900 mb-4">
-                                {event.name}
-                            </h1>
-                            <p className="text-sm text-gray-500 mb-2">
-                                Start：{event.created_at}
-                            </p>
-                            <p className="text-sm text-gray-500 mb-2">
-                                End ： {event.updated_at}
-                            </p>
+                        <div className="flex px-6 py-8 sm:px-10">
+                            <div className="flex flex-col">
+                                <h1 className="text-4xl leading-8 font-bold text-gray-900 mb-4">
+                                    {event.name}
+                                </h1>
+                                <p className="text-sm text-gray-500 mb-2">
+                                    Start：{event.created_at}
+                                </p>
+                                <p className="text-sm text-gray-500 mb-2">
+                                    End ： {event.updated_at}
+                                </p>
+                            </div>
+                            <div className="m-4">
+                                {tags && tags.map((tag) => (
+                                    <button
+                                        key={tag.id}
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
+                                    >
+                                        {tag.name.en}
+                                    </button>
+                                ))}
+                            </div>
                         </div>
 
                         <div className="border-t border-gray-200 px-4 py-5 sm:p-6">
@@ -67,7 +95,7 @@ const EventDetail = ({ auth, event, is_attend, participants }) => {
                                         Limit of Attendance
                                     </dt>
                                     <dd className="mt-1 text-sm text-gray-900">
-                                        {participants.length}/{event.participant_limit_number}
+                                        {participantsState.length}/{event.participant_limit_number}
                                     </dd>
                                 </div>
                                 <div className="sm:col-span-2">
@@ -89,14 +117,14 @@ const EventDetail = ({ auth, event, is_attend, participants }) => {
                                 </a>
                             </div>
 
-                            {!is_attend && (
+                            {!isAttended && (
                                 <div className="pl-2">
                                     <button onClick={handleAttendClick} className="bg-blue-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-blue-600">
                                         attend
                                     </button>
                                 </div>
                             )}
-                            {is_attend && (
+                            {isAttended && (
                                 <div className="pl-2">
                                     <button onClick={handleNotAttendClick} className="bg-red-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-red-600">
                                         not attend
