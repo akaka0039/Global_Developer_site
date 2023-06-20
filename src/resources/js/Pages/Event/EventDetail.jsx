e01e76ef5c9943025ddd7e15a8045cec108d4630
aka0039 <yumeno.13579@gmail.com>
2023-06-20 12:08:53 -0700

    fix layout of detail page

--- src/resources/js/Pages/Event/EventDetail.jsx
+++ src/resources/js/Pages/Event/EventDetail.jsx
@@ -3,22 +3,35 @@ import { router } from "@inertiajs/react";
 import GeneralLayout from "@/Layouts/GeneralLayout";
 import axios from "axios";
 
-const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }) => {
+const EventDetail = ({
+    auth,
+    event,
+    is_attended,
+    participants,
+    tags,
+    wait_list,
+}) => {
     // for debug
     console.log(event, is_attended, participants, tags, wait_list);
+
+    const image = event.image || "";
 
     const [isAttended, SetIsAttended] = useState(is_attended);
     const [participantsState, SetParticipantsState] = useState(participants);
     const [waitListState, SetWaitListState] = useState(wait_list);
-    const isFullyOccupied = participantsState.length >= event.participant_limit_number;
+    const isFullyOccupied =
+        participantsState.length >= event.participant_limit_number;
 
     const handleEditClick = () => {
         // console.log(event.event_id);
         router.get(`/events/${event.event_id}/edit`);
-    }
+    };
 
     const handleAttendClick = async () => {
-        const response = await axios.post(`/events/${event.event_id}/participants`, { user_id: auth?.user?.user_id });
+        const response = await axios.post(
+            `/events/${event.event_id}/participants`,
+            { user_id: auth?.user?.user_id }
+        );
         if (response?.data?.is_attended) {
             SetIsAttended(true);
         }
@@ -28,10 +41,13 @@ const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }
         if (response?.data?.wait_list) {
             SetWaitListState(response?.data?.wait_list);
         }
-    }
+    };
 
     const handleNotAttendClick = async () => {
-        const response = await axios.delete(`/events/${event.event_id}/participants`, { user_id: auth?.user?.user_id });
+        const response = await axios.delete(
+            `/events/${event.event_id}/participants`,
+            { user_id: auth?.user?.user_id }
+        );
         if (!response?.data?.is_attended) {
             SetIsAttended(false);
         }
@@ -41,13 +57,13 @@ const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }
         if (response?.data?.wait_list) {
             SetWaitListState(response?.data?.wait_list);
         }
-    }
+    };
 
     return (
         <GeneralLayout auth={auth}>
-            <div className="flex flex-col justify-center items-center h-full pt-8">
-                <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
-                    <div className="flex justify-end pb-3">
+            <div className="flex flex-col justify-center items-center h-full pt-8 pb-6">
+                <div className="max-w-6xl w-full px-4 sm:px-6 lg:px-8">
+                    <div className="bg-gray-100 flex justify-end pb-3">
                         <button
                             className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded  focus:outline-none focus:shadow-outline"
                             onClick={handleEditClick}
@@ -56,35 +72,27 @@ const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }
                         </button>
                     </div>
                     <div className="bg-white overflow-hidden shadow sm:rounded-lg">
-                        <div className="relative">
-                            <img
-                                className="w-full h-96 object-cover sm:h-80"
-                                src={event.image}
-                                alt="Event Image"
-                            />
-                            <div className="absolute inset-0 bg-black opacity-25"></div>
+                        <div className="h-screen max-h-96 sm:max-h-72 flex items-center">
+                            <div
+                                className={`flex bg-center bg-cover bg-no-repeat border object-contain rounded-md border-gray-200`}
+                                style={{
+                                    backgroundImage: `url(/storage/images/${image})`,
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            ></div>
                         </div>
                         <div className="flex px-6 py-8 sm:px-10">
                             <div className="flex flex-col">
                                 <h1 className="text-4xl leading-8 font-bold text-gray-900 mb-4">
                                     {event.name}
                                 </h1>
-                                <p className="text-sm text-gray-500 mb-2">
-                                    Start：{event.created_at}
+                                <p className="text-md text-gray-600 mb-2">
+                                    Start：{event.start_date}
                                 </p>
-                                <p className="text-sm text-gray-500 mb-2">
-                                    End ： {event.updated_at}
+                                <p className="text-md text-gray-600 mb-2">
+                                    End ： {event.end_date}
                                 </p>
-                            </div>
-                            <div className="m-4">
-                                {tags && tags.map((tag) => (
-                                    <button
-                                        key={tag.id}
-                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
-                                    >
-                                        {tag.name.en}
-                                    </button>
-                                ))}
                             </div>
                         </div>
 
@@ -92,18 +100,50 @@ const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }
                             <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
                                 <div className="sm:col-span-1">
                                     <dt className="text-sm font-medium text-gray-500">
-                                        Location
+                                        {event.is_online === 0
+                                            ? "Location"
+                                            : "Online Event"}
                                     </dt>
                                     <dd className="mt-1 text-sm text-gray-900">
-                                        {event.address}
+                                        <div className="flex items-center">
+                                            <span>{event.address}</span>
+                                            <button
+                                                className="ml-2 p-1 rounded-md bg-gray-200 hover:bg-gray-300 focus:bg-gray-300"
+                                                onClick={() => {
+                                                    navigator.clipboard.writeText(
+                                                        event.address
+                                                    );
+                                                    alert(
+                                                        "Copied to clipboard"
+                                                    );
+                                                }}
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke-width="1.0"
+                                                    stroke="currentColor"
+                                                    className="w-4 h-4"
+                                                >
+                                                    <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
                                     </dd>
                                 </div>
+
                                 <div className="sm:col-span-1">
                                     <dt className="text-sm font-medium text-gray-500">
                                         Limit of Attendance
                                     </dt>
                                     <dd className="mt-1 text-sm text-gray-900">
-                                        {participantsState.length}/{event.participant_limit_number}
+                                        {participantsState.length}/
+                                        {event.participant_limit_number}
                                     </dd>
                                     <dt className="text-sm font-medium text-gray-500">
                                         Number of people waiting
@@ -120,8 +160,25 @@ const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }
                                         {event.description}
                                     </dd>
                                 </div>
+                                <div className="sm:col-span-2">
+                                    <dt className="text-sm font-medium text-gray-500">
+                                        Tag
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900">
+                                        {tags &&
+                                            tags.map((tag) => (
+                                                <button
+                                                    key={tag.id}
+                                                    className="bg-gray-300 hover:bg-gray-600 text-white font-bold py-1 px-3 rounded mt-2 ml-4"
+                                                >
+                                                    {tag.name.en}
+                                                </button>
+                                            ))}
+                                    </dd>
+                                </div>
                             </dl>
                         </div>
+
                         <div className="bg-gray-100 px-4 py-4 sm:px-6 flex justify-end">
                             <div className="pl-2">
                                 <a href="/events">
@@ -133,21 +190,30 @@ const EventDetail = ({ auth, event, is_attended, participants, tags, wait_list }
 
                             {isFullyOccupied && !isAttended && (
                                 <div className="pl-2">
-                                    <button onClick={handleAttendClick} className="bg-orange-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-orange-600">
+                                    <button
+                                        onClick={handleAttendClick}
+                                        className="bg-orange-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-orange-600"
+                                    >
                                         waitlist
                                     </button>
                                 </div>
                             )}
                             {!isFullyOccupied && !isAttended && (
                                 <div className="pl-2">
-                                    <button onClick={handleAttendClick} className="bg-blue-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-blue-600">
+                                    <button
+                                        onClick={handleAttendClick}
+                                        className="bg-blue-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-blue-600"
+                                    >
                                         attend
                                     </button>
                                 </div>
                             )}
                             {isAttended && (
                                 <div className="pl-2">
-                                    <button onClick={handleNotAttendClick} className="bg-red-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-red-600">
+                                    <button
+                                        onClick={handleNotAttendClick}
+                                        className="bg-red-500 text-white rounded-md px-4 py-2 transition duration-300 ease-in-out hover:bg-red-600"
+                                    >
                                         not attend
                                     </button>
                                 </div>
