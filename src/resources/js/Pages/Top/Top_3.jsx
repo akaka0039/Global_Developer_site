3861cbe470ce94ee558b798c9847f83e9b5796ad
aka0039 <yumeno.13579@gmail.com>
2023-06-25 00:41:32 -0700

    fix issue #151

--- src/resources/js/Pages/Top/Top_3.jsx
+++ src/resources/js/Pages/Top/Top_3.jsx
@@ -5,11 +5,11 @@ import Project from "/public/images/Project.png";
 
 function Top_3() {
     return (
-        <div className="flex justify-center items-center bg-white pt-6">
+        <div className="flex justify-center items-center bg-white pt-3">
             <div className="w-full md:w-10/12 lg:w-9/12 xl:w-9/12">
                 <div className="text-center">
                     <div className="mt-8 flex flex-col sm:flex-row justify-center items-center">
-                        <div className="rounded-full bg-gray-300 p-2 mx-4 mb-4 sm:mb-0">
+                        <div className="rounded-full bg-blue-300 p-2 mx-4 mb-4 sm:mb-0">
                             <img
                                 src={Project}
                                 alt="Project"
@@ -19,16 +19,14 @@ function Top_3() {
                                     objectPosition: "center",
                                 }}
                             />
-                            <h2 className="text-2xl font-bold mt-4">
-                                Project Image
-                            </h2>
+                            <h2 className="text-4xl font-bold mt-4">Project</h2>
                             <p className="text-gray-600 mt-2">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Sed feugiat vestibulum nunc in
-                                tincidunt. Vivamus vel lacus dolor.
+                                Collaboratively launching and developing
+                                projects within our community to enhance skills
+                                and leverage for career opportunities.
                             </p>
                         </div>
-                        <div className="rounded-full bg-blue-300 p-2 mx-4 ">
+                        <div className="rounded-full bg-gray-300 p-2 mx-4 ">
                             <img
                                 src={JubHunting}
                                 alt="JubHunting"
@@ -38,16 +36,16 @@ function Top_3() {
                                     objectPosition: "center",
                                 }}
                             />
-                            <h2 className="text-2xl font-bold mt-4">
-                                JubHunting Image
+                            <h2 className="text-4xl font-bold mt-4">
+                                JubHunting
                             </h2>
                             <p className="text-gray-600 mt-2">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Sed feugiat vestibulum nunc in
-                                tincidunt. Vivamus vel lacus dolor.
+                                Leverage our vibrant community for job hunting,
+                                where connections, resources, and support propel
+                                you towards career success.
                             </p>
                         </div>
-                        <div className="rounded-full bg-gray-300 p-2 mx-4 mb-4 sm:mb-0">
+                        <div className="rounded-full bg-blue-300 p-2 mx-4  my-4">
                             <img
                                 src={Self}
                                 alt="Self"
@@ -57,13 +55,11 @@ function Top_3() {
                                     objectPosition: "center",
                                 }}
                             />
-                            <h2 className="text-2xl font-bold mt-4">
-                                Self Image
-                            </h2>
+                            <h2 className="text-4xl font-bold mt-4">Self</h2>
                             <p className="text-gray-600 mt-2">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Sed feugiat vestibulum nunc in
-                                tincidunt. Vivamus vel lacus dolor.
+                                Grow personally and professionally through
+                                community events, fostering learning, skill
+                                development, and a supportive network.
                             </p>
                         </div>
                     </div>
